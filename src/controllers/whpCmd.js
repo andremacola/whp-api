81a4b2a4b25916e8be7c043bf3ae4d8630da2fc2
Andre Macola <andremacola@gmail.com>
2020-06-10 22:13:29 -0300

    dolar and stock custom commands

--- src/controllers/whpCmd.js
+++ src/controllers/whpCmd.js
@@ -1,3 +1,5 @@
+const axios = require('axios').default;
+
 class whpCmd {
 	bad() {
 		return false;
@@ -22,15 +24,60 @@ class whpCmd {
 			case (body == '@ping'):
 				client.sendText(number, '@pong');
 				break;
+			case (body.startsWith('@ac ')):
+				this.stock(client, number, body);
+				break;
+			case (body == '@usd'):
+				this.brl(client, number, 'USD');
+				break;
+			case (body == '@eur'):
+				this.brl(client, number, 'EUR');
+				break;
 			default:
 				break;
 		}
 	}
 
+	static brl(client, number, coin = 'USD') {
+		axios
+			.get(`https://economia.awesomeapi.com.br/${coin}`)
+			.then((res) => {
+				const { name, bid, ask, pctChange } = res.data[0];
+				const icTitle = (pctChange[0] === '-') ? '📉' : '📈';
+				const icVar = (pctChange[0] === '-') ? '🔻' : '🔼';
+				const msg = `${icTitle} *${name}* ${icTitle}\n*Compra:* R$${bid}\n*Venda:* R$${ask}\n*Variação:* ${icVar} ${pctChange}%`;
+				client.sendText(number, msg);
+			});
+	}
+
+	static stock(client, number, stock) {
+		const ac = stock.slice(4);
+		axios
+			.get(`https://apidojo-yahoo-finance-v1.p.rapidapi.com/market/get-quotes`,
+				{
+					params: {
+						symbols: `${ac}.SA`,
+						region: 'BR',
+						lang: 'pt-BR',
+					},
+					headers: {
+						'x-rapidapi-host': 'apidojo-yahoo-finance-v1.p.rapidapi.com',
+						'x-rapidapi-key': '3148a30e3cmshcc97bfcd0f10725p10df16jsn3cb9d1db3f7d',
+					},
+				},
+			)
+			.then((res) => {
+				const { longName, symbol, regularMarketPrice, regularMarketChange } = res.data.quoteResponse.result[0];
+				const icTitle = (regularMarketChange < 0) ? '📉' : '📈';
+				const icVar = (regularMarketChange < 0) ? '🔻' : '🔼';
+				const msg = `${icTitle} *${longName}* ${icTitle}\n*ID:* ${symbol}\n*Preço:* ${icVar} R$${regularMarketPrice}\n*Variação:* ${icVar} ${regularMarketChange}%`;
+				client.sendText(number, msg);
+			});
+	}
+
 	static outLine(client, number, body, replyMsg) {
 		const sourceUrl = body.replace('@pay ', '');
 		const outline = `https://outline.com/${sourceUrl}`;
-		// const outline = `https://api.outline.com/v3/parse_article?source_url=${sourceUrl}`;
 		if (sourceUrl) {
 			client.reply(number, outline, replyMsg, true);
 		}
